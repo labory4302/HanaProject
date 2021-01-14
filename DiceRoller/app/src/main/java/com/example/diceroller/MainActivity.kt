5a8fd13bfb032b0a5faa0788cd068f7dcd77b573
brand4302 <brand4302@summitcorp.co.kr>
2021-01-14 20:26:00 +0900

    주사위 굴리기 기능 구현

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.rollButton)
+
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val resultText: TextView = findViewById(R.id.resultText)
+        val randomNum = Random.nextInt(1, 7)
+        resultText.text = randomNum.toString()
     }
 }
