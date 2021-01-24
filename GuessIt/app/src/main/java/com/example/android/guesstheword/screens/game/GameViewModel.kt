298f126b6d20646c3c0a424ed985cb250cfcb0e2
brand4302 <brand4302@summitcorp.co.kr>
2021-01-24 19:40:51 +0900

    UI 컨트롤러에서 Data의 수정을 막도록 캡슐화 시키기

--- GuessIt/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ GuessIt/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,16 +1,23 @@
 package com.example.android.guesstheword.screens.game
 
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
 
-    // The current word
-    var word = MutableLiveData<String>()
+    // 단어
+    // 캡슐화 시키기(Fragment에서 수정을 못하도록 막기)
+    var _word = MutableLiveData<String>()
+    val word : LiveData<String>
+        get() = _word
 
-    // The current score
-    var score = MutableLiveData<Int>()
+    // 점수
+    // 캡슐화 시키기(Fragment에서 수정을 못하도록 막기)
+    private var _score = MutableLiveData<Int>()
+    val score : LiveData<Int>
+        get() = _score
 
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
@@ -19,7 +26,7 @@ class GameViewModel : ViewModel() {
     init {
         resetList()
         nextWord()
-        score.value = 0
+        _score.value = 0
     }
 
     private fun resetList() {
@@ -54,17 +61,17 @@ class GameViewModel : ViewModel() {
         if (wordList.isEmpty()) {
             //gameFinished()
         } else {
-            word.value = wordList.removeAt(0)
+            _word.value = wordList.removeAt(0)
         }
     }
 
     fun onSkip() {
-        score.value = (score.value)?.minus(1)
+        _score.value = (score.value)?.minus(1)
         nextWord()
     }
 
     fun onCorrect() {
-        score.value = (score.value)?.plus(1)
+        _score.value = (score.value)?.plus(1)
         nextWord()
     }
 }
