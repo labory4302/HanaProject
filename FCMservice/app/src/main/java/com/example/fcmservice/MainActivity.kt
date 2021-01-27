42457df0493601f660c4d8c65244d906c1f44ab8
brand4302 <brand4302@summitcorp.co.kr>
2021-01-27 17:51:31 +0900

    토큰 로그 보기

--- FCMservice/app/src/main/java/com/example/fcmservice/MainActivity.kt
+++ FCMservice/app/src/main/java/com/example/fcmservice/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.fcmservice
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val pref = this.getSharedPreferences("token", Context.MODE_PRIVATE)
+        val token = pref.getString("token", "--")
+        Log.d("token : ", token.toString())
     }
 }
